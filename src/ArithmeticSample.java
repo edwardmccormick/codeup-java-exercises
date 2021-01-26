3256875dd5a6a849c2ff27d676faf79404387fc3
edwardmccormick <ted.mccormick@gmail.com>
2021-01-26 09:31:59 -0600

    Added a couple of classes because of the quiz

---
+++ src/ArithmeticSample.java
@@ -0,0 +1,10 @@
+public class ArithmeticSample {
+    public static void main(String[] args) {
+        int x = 5 * 4 % 3;
+        System.out.println(x);
+
+        String str = "Can you hear me now?";
+
+        System.out.println(x + str);
+    }
+}
