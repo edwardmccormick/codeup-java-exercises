754b2e699ea8110b1ec5e96c0e97054737e9974f
edwardmccormick <ted.mccormick@gmail.com>
2021-01-31 21:06:50 -0600

    Updating exercises

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,7 +3,7 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    Scanner scanner = new Scanner();
+    Scanner scanner = new Scanner(System.in);
 
     public String getString() {
         System.out.println("Please enter a string.");
@@ -11,7 +11,7 @@ public class Input {
         return input;
     }
 
-    public boolean boolean() {
+    public boolean trueOrFalse() {
         System.out.println("Please enter either yes (for true) or no (for false)");
         String input = scanner.nextLine();
         return (input.equalsIgnoreCase("yes") | input.equalsIgnoreCase("y") | input.equalsIgnoreCase("true") | input.equalsIgnoreCase("t"));
